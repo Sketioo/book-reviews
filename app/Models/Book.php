eff327258a8da7c350c5057744f8a2f7ba6fc853
AmurTigro <martiohusein27@gmail.com>
2024-07-30 20:54:25 +0700

    Created date filter for scope method in Book model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use App\Models\Review;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+// use Illuminate\Database\Query\Builder;
 
 class Book extends Model
 {
@@ -13,5 +16,42 @@ class Book extends Model
     public function reviews()
     {
         return $this->hasMany(Review::class);
+    }
+
+    //* Local Scope Query
+    public function scopeTitle(Builder $query, string $title): Builder
+    {
+        return $query->where('title', 'like', '%' . $title . '%');
+    }
+
+    public function scopePopular(Builder $query, $from = null, $to = null): Builder
+    {
+        return $query->withCount([
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to),
+        ])->orderBy('reviews_count', 'desc');
+    }
+
+    public function scopeHighestRated(Builder $query, $from = null, $to = null)
+    {
+        return $query->withAvg([
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ], 'rating')
+            ->orderBy('reviews_avg_rating', 'desc');
+    }
+
+    public function scopeMinReviews(Builder $query, int $minReviews)
+    {
+        return $query->having('reviews_count', '>', $minReviews);
+    }
+
+    private function dateRangeFilter(Builder $query, $from = null, $to = null)
+    {
+        if($from && !$to) {
+            $query->where('created_at', '>=', $from);
+        } elseif(!$from && $to) {
+            $query->where('created_at', '<=', $to);
+        } elseif (!$from && !$to) {
+            $query->whereBetween('created_at', [$from, $to]);
+        }
     }
 }
